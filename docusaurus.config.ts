cada963abf99fe82632dcb0c4658d6a0ae44afc4
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2024-03-07 12:21:03 +0100

    openapi docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import * as path from "path";
 
 const config: Config = {
   title: "Buildel",
@@ -16,8 +17,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "buildel", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -33,25 +34,72 @@ const config: Config = {
   presets: [
     [
       "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
       {
         docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          docItemComponent: "@theme/ApiItem", // derived from docusaurus-theme-openapi-docs
+        },
+        blog: {
+          showReadingTime: true,
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: require.resolve("./src/css/custom.css"),
         },
-      } satisfies Preset.Options,
+      },
     ],
   ],
+
+  plugins: [
+    () => ({
+      name: "resolve-react",
+      configureWebpack() {
+        return {
+          resolve: {
+            alias: {
+              // assuming root node_modules is up from "./packages/<your-docusaurus>
+              react: path.resolve("../../node_modules/react"),
+            },
+          },
+        };
+      },
+    }),
+    "docusaurus-plugin-sass",
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "openapi", // plugin id
+        docsPluginId: "classic", // id of plugin-content-docs or preset for rendering docs
+        config: {
+          workflows: {
+            // the <id> referenced when running CLI commands
+            specPath: "openapi.json", // path to OpenAPI spec, URLs supported
+            outputDir: "docs/api/workflows", // output directory for generated files
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag",
+              // optional, instructs plugin to generate sidebar.js
+              // groupPathsBy: "tag", // group sidebar items by operation "tag"
+            },
+          },
+        },
+      },
+    ],
+  ],
+  themes: ["docusaurus-theme-openapi-docs"],
 
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
     navbar: {
       logo: {
         alt: "My Site Logo",
@@ -65,11 +113,15 @@ const config: Config = {
           label: "Documentation",
         },
         {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
-          position: "left",
+          type: "dropdown",
           label: "API Reference",
-          href: "/api-reference",
+          position: "left",
+          items: [
+            {
+              label: "API Zoo",
+              to: "/api/workflows",
+            },
+          ],
         },
         {
           href: "https://github.com/elpassion/buildel",
--- src/pages/api-reference.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from "@theme/Layout";
-
-export default function ApiReference() {
-  return <Layout>API reference</Layout>;
-}
