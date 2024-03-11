a18466f639eafbe232ec0224b1799575812f4391
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2024-03-11 16:42:19 +0100

    update cta button on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Hero } from "@site/src/components/Hero";
 
 export default function Home() {
   return (
-    <Layout description="Description will go into a meta tag in <head />">
+    <Layout>
       <Hero />
     </Layout>
   );
