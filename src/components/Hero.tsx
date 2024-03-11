3d6f3a7de89e2f21acdda2595def36bc49e9a704
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2024-03-11 12:58:26 +0100

    drop unnecessary components

---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import clsx from "clsx";
+import styles from "@site/src/pages/index.module.css";
+import Heading from "@theme/Heading";
+import Link from "@docusaurus/Link";
+
+export const Hero = () => {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Read docs
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
