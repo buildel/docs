75b26dd244fd1d016296d713be2683f29cfc9339
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-08-26 15:02:46 +0200

    update docs url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/b-favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://docs.buildel.ai",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
